0e98dd1d4ee3e7774e16d83966927b468d6de1d7
max <fantomax0@gmail.com>
2015-12-06 15:50:47 +0300

    added producer_id to get_all_goods

--- models/goods.py
+++ models/goods.py
@@ -3,8 +3,8 @@ from models.db.db import Db
 
 class Goods(Db):
     def get_all_goods(self):
-        sql = 'SELECT id, name, price, weight, residue FROM Goods'
+        sql = 'SELECT id, name, price, producer_id, weight, residue FROM Goods'
         cursor = self.execute(sql)
-        data = self.get_dict_list(['id', 'name', 'price', 'weight', 'residue'], cursor)
+        data = self.get_dict_list(['id', 'name', 'price', 'producer_id', 'weight', 'residue'], cursor)
         cursor.close()
         return data
