0df2e1de00a8076febf44886ac490456920fa012
Client <client.pas.95@gmail.com>
2015-12-06 21:06:07 +0300

    Added addition of good.

--- windows/controllers/organization_interface/good_adder.py
+++ windows/controllers/organization_interface/good_adder.py
@@ -1,5 +1,6 @@
 from PyQt4 import QtGui, QtCore, uic
 from windows.widgets import ADD_GOOD_FORM
+from models.organizations import Organizations
 
 
 class GoodAdder(QtGui.QWidget):
@@ -11,6 +12,7 @@ class GoodAdder(QtGui.QWidget):
         uic.loadUi(self._path, self)
         stacked_widget.addWidget(self)
         self.ancestor = args[0]
+        self.organization = Organizations()
 
         self.add.clicked.connect(self.on_add)
         self.cancel.clicked.connect(self.on_cancel)
@@ -19,5 +21,23 @@ class GoodAdder(QtGui.QWidget):
         self.ancestor.on_cancel()
 
     def on_add(self):
-        pass
-        # validation of data in form and creating good
+        name = self.name.text()
+        price = self.price.text()
+        weight = self.weight.text()
+        residue = self.residue.text()
+        try:
+            price = int(price)
+            weight = int(weight)
+            residue = int(residue)
+            self.organization.add_goods(
+                name,
+                price,
+                self.parent().organization_id,
+                residue,
+                weight
+            )
+            self.on_cancel()
+            return
+        except Exception as e:
+            self.error.setText('Something wrong with good.')
+
