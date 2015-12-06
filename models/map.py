1bdd989ab5db46b6b39e85d229211a7b6e1ae9df
max <fantomax0@gmail.com>
2015-12-06 21:19:34 +0300

    added get_city and get_city_id in map.py

--- models/map.py
+++ models/map.py
@@ -16,3 +16,21 @@ class Map(Db):
         data = self.get_dict_list(['start_city_id', 'finish_city_id', 'length'], cur)
         cur.close()
         return data
+
+    def get_city(self, city_id):
+        query = 'SELECT name FROM Cities WHERE id = %s'
+        cur = self.execute(query, (city_id,))
+        data = cur.fetchall()
+        cur.close()
+        if len(data) == 0:
+            raise Exception('no such city id')
+        return data[0][0]
+
+    def get_city_id(self, city_name):
+        query = 'SELECT id FROM Cities WHERE name = %s'
+        cur = self.execute(query, (city_name,))
+        data = cur.fetchall()
+        cur.close()
+        if len(data) == 0:
+            raise Exception('no such city name')
+        return data[0][0]
