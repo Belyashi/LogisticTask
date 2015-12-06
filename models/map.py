aded81338f6b8c96e1997ef5e0016cf93e403e34
alexejlapotko <alexej.lapotko@gmail.com>
2015-12-06 14:07:06 +0300

    Added get all cities method

--- models/map.py
+++ models/map.py
@@ -4,3 +4,12 @@ from db import Db
 class Map(Db):
     def get_route(self):
         pass
+
+    def get_all_cities(self):
+        cur = self.execute('SELECT id, name FROM Cities')
+        data = [{
+                    'id': id,
+                    'name': name
+                } for id, name in cur]
+        cur.close()
+        return data
--- models/test.py
+++ models/test.py
@@ -1,5 +1,6 @@
 from users import Users
 from drivers import Drivers
+from map import Map
 import create_db
 
 
@@ -7,30 +8,44 @@ def recreate_db():
     create_db.create_db()
     create_db.create_tables()
 
+
 d = Drivers()
-# d.assign_order(2, 1)
-# d.assign_order(2, 2)
-# d.assign_order(3, 3)
-# print d.get_orders(1)
-# print d.get_orders(2)
-# print d.get_orders(3)
 
-# print d.get_available_drivers(1)
-# print d.get_available_drivers(2)
-# print d.get_available_drivers(3)
 
-# print d.get_available_drivers(2)
-# d.start_move(1)
-# print d.get_next_point(1)
-# d.arrive_to_point(1)
-# print d.get_next_point(1)
-# print d.get_available_drivers(2)
-# d.start_move(1)
-# print d.get_available_drivers(2)
-#
-# print d.get_next_point(2)
-# print d.get_next_point(3)
+def test_drivers_assign_order():
+    d.assign_order(2, 1)
+    d.assign_order(2, 2)
+    d.assign_order(3, 3)
+    print d.get_orders(1)
+    print d.get_orders(2)
+    print d.get_orders(3)
 
-print d.get_next_point(1)
-d.start_move(1)
-d.arrive_to_point(1)
+
+def test_drivers_move_1():
+    print d.get_available_drivers(1)
+    print d.get_available_drivers(2)
+    print d.get_available_drivers(3)
+
+    d.start_move(1)
+    print d.get_next_point(1)
+    d.arrive_to_point(1)
+    print d.get_next_point(1)
+    print d.get_available_drivers(2)
+    d.start_move(1)
+    print d.get_available_drivers(2)
+
+
+def test_drivers_move_2():
+    print d.get_next_point(2)
+    print d.get_next_point(3)
+
+    print d.get_next_point(1)
+    d.start_move(1)
+    d.arrive_to_point(1)
+
+
+m = Map()
+
+
+def test_map_get_all_cities():
+    print m.get_all_cities()
