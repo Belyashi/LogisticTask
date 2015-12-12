64b9634136f496ce14f12f8cdaf67e5730f0d341
Client <client.pas.95@gmail.com>
2015-12-12 04:26:36 +0300

    Undo and another update of tables.

--- models/sql_scripts/test_data.sql
+++ models/sql_scripts/test_data.sql
@@ -11,13 +11,13 @@ INSERT INTO Ways (start_city_id, finish_city_id, length) VALUES (2, 3, 200);
 INSERT INTO Ways (start_city_id, finish_city_id, length) VALUES (2, 4, 300);
 INSERT INTO Ways (start_city_id, finish_city_id, length) VALUES (3, 5, 10);
 
---passwords
-INSERT INTO Users (login, pass) VALUES ("vasia", "78d8045d684abd2eece923758f3cd781489df3a48e1278982466017f");  --123
-INSERT INTO Users (login, pass) VALUES ("masha", "99fb2f48c6af4761f904fc85f95eb56190e5d40b1f44ec3a9c1fa319");  --1234
-INSERT INTO Users (login, pass) VALUES ("petia", "99fb2f48c6af4761f904fc85f95eb56190e5d40b1f44ec3a9c1fa319");  --321
-INSERT INTO Users (login, pass) VALUES ("sasha", "2b88818c9e8511ae74d9add26c7e9d5380a2ae7c2eee4d9ca84d0649");  --111
-INSERT INTO Users (login, pass) VALUES ("ivan", "1dc1c160284a5b1d5cbb04f6856c4c34806d33ceee7158b426821beb");   --121
-INSERT INTO Users (login, pass) VALUES ("userus", "ea8fd8d3be56627a2dbca5c0dabb2cc14190e20f45ebd2c0b33b6e44"); --4321
+##passwords
+INSERT INTO Users (login, pass) VALUES ("vasia", "78d8045d684abd2eece923758f3cd781489df3a48e1278982466017f");  ##123
+INSERT INTO Users (login, pass) VALUES ("masha", "99fb2f48c6af4761f904fc85f95eb56190e5d40b1f44ec3a9c1fa319");  ##1234
+INSERT INTO Users (login, pass) VALUES ("petia", "fa537e8a03f79fb79f065eb12e84df781537f3c7f2a7c16069f90341");  #321
+INSERT INTO Users (login, pass) VALUES ("sasha", "2b88818c9e8511ae74d9add26c7e9d5380a2ae7c2eee4d9ca84d0649");  #111
+INSERT INTO Users (login, pass) VALUES ("ivan", "1dc1c160284a5b1d5cbb04f6856c4c34806d33ceee7158b426821beb");   #121
+INSERT INTO Users (login, pass) VALUES ("userus", "ea8fd8d3be56627a2dbca5c0dabb2cc14190e20f45ebd2c0b33b6e44"); #4321
 
 INSERT INTO Drivers (user_id, capacity, last_city_id) VALUES (1, 100, 1);
 INSERT INTO Drivers (user_id, capacity, last_city_id) VALUES (3, 150, 2);
